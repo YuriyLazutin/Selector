d27ffb21620181e67c79d6ebe6f9b0c91fe7b462
Yuriy Lazutin <yuriylazutin@gmail.com>
2022-03-21 18:40:43 +0700

    Improved connection tool. After this changes connection with Oracle was successfully via 3 ways (host-based, TNS-based, and connect string-based)

--- connect-tool/dboracle.cpp
+++ connect-tool/dboracle.cpp
@@ -23,25 +23,12 @@ DBOracle::~DBOracle()
 
 void DBOracle::Connect2Srv(QString user, QString passwd, QString db)
 {
- // std::string u = user.toStdString().c_str();
- // std::string p = passwd.toStdString();
-//  std::string d = db.toStdString();
-  string usr = user.toStdString();
-  string pswd = passwd.toStdString();
-  string dbo = db.toStdString();
-
-  cout << usr << " " << usr.size() << endl;
-  cout << pswd << " " << pswd.size() << endl;
-  cout << dbo << " " << dbo.size() << endl;
-
-//  try
-//  {
-    //conn = env->createConnection(usr, pswd, dbo);
-    conn = env->createConnection("usr", "pswd", "dbo");
-//  }
-//  catch (SQLException ex)
-//  {
-//    cout << "Exception catched!" << endl;
-    //cout << ex.getMessage() << endl;
-//  }
+  try
+  {
+    conn = env->createConnection(user.toStdString(), passwd.toStdString(), db.toStdString());
+  }
+  catch (SQLException ex)
+  {
+    cout << ex.getMessage() << endl;
+  }
 }
